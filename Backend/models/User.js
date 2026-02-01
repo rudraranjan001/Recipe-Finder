df50d380aca7d2bdd0a664061247cde58bfbe6a9
Rudra <rudraranjan2003@gmail.com>
2026-02-01 17:58:18 +0530

    add middleware hook

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema(
     {
@@ -21,5 +22,19 @@ const userSchema = new mongoose.Schema(
         timestamps : true,
     }
 );
+
+
+//pre-save hook
+userSchema.pre('save',async function(next){
+
+    if(!this.isModified('password')){
+        return next();
+    }
+
+    const salt = await bcrypt.genSalt(10);
+
+    this.password  = await bcrypt.hash(this.password,salt);
+    next();
+})
 
 module.exports = mongoose.model('User',userSchema);
