890073f21520be3c2f817f09894b9fbef0d04065
Rudra <rudraranjan2003@gmail.com>
2026-02-07 00:30:38 +0530

    completed search bar and added some tailwind

--- Frontend/client/src/pages/HomePage.jsx
+++ Frontend/client/src/pages/HomePage.jsx
@@ -2,26 +2,49 @@ import React, { useState } from 'react';
 import SearchBar from '../components/SearchBar';
 //This is the page where all the user search for the recipes
 import { searchRecipe } from '../services/recipeService';
+import RecipeCard from '../components/RecipeCard';
+
+
 
 const HomePage = () => {
 
     const [recipes,setRecipes] = useState([]);
-
+    const [search,setSearch] = useState(false);
+    
     const handleSearch = async(query) => {
         console.log(`Searching for recipe with query :${query}`);
 
         const results = await searchRecipe(query);
         setRecipes(results);  
+        setSearch(true);
     };
 
     console.log('Current recipes in state :',recipes);
     
     return (
-        <div>
+        <div className="max-w-[1200px] mx-auto p-8 text-center">
             <h1>Recipe Home Page</h1>
             <p>Search for your favourite recipes here!</p>
             <SearchBar onSearch = {handleSearch}/>
+            <div className="grid grid-cols-[repeat(auto-fill,minmax(280px,1fr))] gap-8 mt-8 text-left">
+                {
+                    recipes.map(recipe =>(
+
+                        <RecipeCard key = {recipe.idMeal} recipe = {recipe} />
+                    ))
+                }
+            </div>
+                    {/* Adding a user friendly msg for when no result are found */}
+                    {search && recipes.length === 0 &&  (
+                        <p className="mt-8 text-[1.2rem] text-[#666]">
+                        No recipes found. Please try a different search term!
+                      </p>
+      
+                    )}
+                
+             
         </div>
+        
     );
 };
 
