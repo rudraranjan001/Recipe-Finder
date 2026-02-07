170526ad4afbd4e25c182857ffa0111a580a3d13
Rudra <rudraranjan2003@gmail.com>
2026-02-07 14:11:35 +0530

    making card to functional

--- Frontend/client/src/pages/RecipePage.jsx
+++ Frontend/client/src/pages/RecipePage.jsx
@@ -16,10 +16,12 @@ export default function RecipePage() {
     const fetchRecipeDetails = async () => {
       try{
         setLoading(true); //ensure loading is true at the start of the fetch
+        const data = await getRecipeById(recipeId);
         setRecipe(data);
       }
       catch(error){
-
+          console.error('Failed to fetch recipe details',error);
+          
       }
       finally{
 
@@ -27,12 +29,65 @@ export default function RecipePage() {
         // fetch attempt is complete, whether it succeeded or failed.
         setLoading(false);
       }
+    };
+    fetchRecipeDetails();//calling async function
+  },[recipeId])
+
+  //Data Transformation logic
+  //Creating the clean array of ingredients from the raw recipe object
+  const ingredients = [];
+  if(recipe){
+    for(let i = 1;i<= 20;i++){
+      const ingredient = recipe[`strIngredient${i}`];
+      const measure = recipe[`strMeasure${i}`];
+    
+
+    if(ingredient){
+      ingredients.push({ ingredient , measure });
     }
-  })
+    else{
+      break;
+    }
+  }
+ }
+  
+  if(loading){
+    return<div className="loading-message">Loading recipe...</div>
+
+  }
+
+  if(!recipe){
+    return <div className="error-message">Recipe not found. </div>
+  }
+
   return (
-    <div>
-        <h1>Recipe Detail Page</h1>
-        <p>Details for  recipe ID :<strong>{ recipeId }</strong></p>
+    <div className='recipe-page'>
+        <h1 className='recipe-page-title'>{recipe.strMeal}</h1>
+        <div className="recipe-layout">
+          <div className="recipe-image-container">
+            <img src = {recipe.strMealThumb} alt={recipe.strMeal} className='recipe-page-image' />
+          </div>
+          
+          <div className="recipe-info-container">
+            <h2>Ingredients</h2>
+            <ul className="ingredient-list">
+              {ingredients.map((item,index) =>(
+                <li key = {index}>
+                  <strong>{item.ingredient}</strong> - {item.measure}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+
+        <div className="recipe-instruction">
+          <h2>Instructions</h2>
+          {recipe.strInstructions.Split('\n').map((line,index)=>(
+            line.trim() && <p key = {index}>{line}</p>
+          ))}
+        </div>
+
+
     </div>
 
   );
