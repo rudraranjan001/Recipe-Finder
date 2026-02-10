f71669d6b9cc54bb1531001329815e3ab33877bf
Rudra <rudraranjan2003@gmail.com>
2026-02-11 02:04:29 +0530

    implementing addFavorite Controller

--- Backend/routes/favoriteRoutes.js
+++ Backend/routes/favoriteRoutes.js
@@ -2,19 +2,48 @@ const express = require('express');
 
 const { protect } = require('../middleware/authMiddleware');
 
+const User = require('../models/User');
+const asyncHandler = require('express-async-handler');
+
 const router = express.Router();
 
 const getFavorites = (req,res) =>{
     res.send('GET / api/favorites route - This will fetch all the favorite recipes for the logged-in user.')
 };
 
-const addFavorite = (req,res) =>{
-    res.send(`POST /api/Favorites route - This will add a Favorite for user ${req,user.name}.`)
-}
+const addFavorite = asyncHandler(async(req,res) =>{
+    const { recipeId } = req.body;
+
+    if(!recipeId){
+        res.status(400);
+        throw new Error('Recipe ID is required');
+    }
+    const user = await User.findById(req.user._id);
+
+    if(user){
+        if(user.favorites.includes(recipeId)){
+            res.status(400);
+            throw new Error('Recipe is already in favorites');
+        }
+    
+
+        user.favorites.push(recipeId);
+        await user.save();
+
+        res.status(201).json({
+            message: 'Recipe added to favorite successfully',
+            favorites: user.favorites,
+        });
+    }
+    else{
+        res.status(404);
+        throw new Error('User not found');
+    }
+})
 
 const removeFavorite = (req,res)  => {
     const { recipeId }  = req.params;
-    res.send(`DELETE /api/favorites/${recipeId} route - This will remove a favorite.`)
+    res.send(`DELETE /api/favorites/${recipeId} route`)
 }
 
 router.route('/')
