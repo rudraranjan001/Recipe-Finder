4b886044391040dd7ac5698b9cb4a9dafdccdd11
Rudra <rudraranjan2003@gmail.com>
2026-02-17 02:07:25 +0530

    create favoriteRecipe page

--- Frontend/client/src/components/ProtectedRoute.jsx
+++ Frontend/client/src/components/ProtectedRoute.jsx
@@ -21,7 +21,6 @@ export default function ProtectedRoute({ children }) {
 
     }
 
-  return (
-    children
-  )
+  return  children;
+  
 }
---
+++ Frontend/client/src/pages/FavoritesPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function FavoritesPage() {
+  return (
+    <div className="max-w-[1200px] mx-auto p-8 text-center">
+        <h1 className='mb-4 text-[2.5rem] text-[#333]'>
+            My Favorite Recipes
+            
+        </h1>
+        <p className='text-[1.1rem] text-[#666]'>This is where you saved recipe will appear once you've  added some!</p>
+    </div>
+  )
+}
