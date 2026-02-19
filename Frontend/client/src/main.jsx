77748267eb0a86a65b871066d211ee46a0f633ec
Rudra <rudraranjan2003@gmail.com>
2026-02-19 07:03:57 +0530

    adding MUI into main.jsx

--- Frontend/client/src/main.jsx
+++ Frontend/client/src/main.jsx
@@ -6,13 +6,22 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext.jsx';
+import { ThemeProvider , createTheme } from '@mui/material';
+import CssBaseline from '@mui/material';
 
-createRoot(document.getElementById('root')).render(
+const theme = createTheme({});
+const root = createRoot(document.getElementById('root'));
+
+root.render(
   <StrictMode>
     <BrowserRouter>
+      <ThemeProvider theme = {theme}>
+        <CssBaseline />
         <AuthProvider >
           <App />
          </AuthProvider> {/*by wrapping the entire app component within AuthProvider makes the authentication context(user,loading,login,logout) for every components  */}
+      </ThemeProvider>
+
     </BrowserRouter>
   </StrictMode>,
 )
