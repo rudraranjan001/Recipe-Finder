b7f372f64d85d2769d56228765b4d87cdc973221
Rudra <rudraranjan2003@gmail.com>
2026-02-22 12:14:49 +0530

    create LoadingSpinner

---
+++ Frontend/client/src/components/LoadingSpinner.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Box , CircularProgress } from '@mui/material'
+
+
+export default function LoadingSpinner() {
+  return (
+    <Box
+        sx = {{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            minHeight: '80vh',
+        }}
+    >
+        <CircularProgress />
+    </Box>
+  )
+}
